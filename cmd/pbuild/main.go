39349eb13a28959f6da0d329402950ce5a070d85
Jaden Weiss <jadr2ddude@gmail.com>
2018-05-19 16:51:21 -0400

    add pbuild config

--- cmd/pbuild/main.go
+++ cmd/pbuild/main.go
@@ -1,3 +1,21 @@
 package main
 
+import "github.com/panux/builder/pkgen"
+
 func main() {}
+
+// Config is the configuration struct
+var Config struct {
+	HTTPAddr        string              `json:"http"`
+	Branches        []string            `json:"branches"`
+	Static          string              `json:"static"`
+	LogDir          string              `json:"logs"`
+	BuildManager    string              `json:"manager"`
+	BuildManagerKey string              `json:"managerKey"`
+	CacheDir        string              `json:"cache"`
+	OutputDir       string              `json:"output"`
+	GitDir          string              `json:"gitDir"`
+	GitRepo         string              `json:"gitRepo"`
+	Arch            pkgen.ArchSet       `json:"arch"`
+	Parallel        map[pkgen.Arch]uint `json:"parallel"`
+}
