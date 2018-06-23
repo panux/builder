a660540d9f1d3c3bf6215cad1705fa55f48b9c20
Jaden Weiss <jadr2ddude@gmail.com>
2018-06-23 12:08:19 -0400

    improve container bootstrap process

--- cmd/worker/bootstrap.sh
+++ cmd/worker/bootstrap.sh
@@ -18,7 +18,42 @@ tar -xf /root/pkgs/lpkg.tar.* -C /
 # create database dir
 mkdir -p /etc/lpkg.d/db
 
+INSTALLEDPKGS=
+
+infoval() {
+    (
+        . "$1"
+        local val="$2"
+        eval "echo \$$val"
+    ) || return $?
+}
+
+mkdir /tmp/inst
+
+installpkg() {
+    for i in $INSTALLEDPKGS; do
+        # if it is already on the list, skip it
+        if [ $i == $1 ]; then
+            return
+        fi
+    done
+
+    # add to installed list
+    INSTALLEDPKGS="$INSTALLEDPKGS $1"
+
+    # install dependencies first
+    tar -xOf /root/pkgs/$1.tar.* ./.pkginfo > /tmp/inst/$1.pkginfo
+    for i in $(infoval /tmp/inst/$1.pkginfo DEPENDENCIES); do
+        installpkg $i
+    done
+
+    # install
+    sh /usr/bin/lpkg-inst /root/pkgs/$1.tar.*
+}
+
 # install packages
 for i in $(cat /root/pkgs/inst.list); do
-    sh /usr/bin/lpkg-inst /root/pkgs/$i.tar.*
+    installpkg $i
 done
+
+rm -r /tmp/inst
